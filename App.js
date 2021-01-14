42462a3fbc892922933ea5f0e73c470ae8e7684f
Tyler Farhner <tylerfarhner@gmail.com>
2021-01-13 22:20:18 -0800

    accepts todo item, adds to state

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [ todoItem, setTodoItem ] = useState('');
+  const [ todoList, setTodoList ] = useState([]);
+
+  const addTodoList = () => {
+    setTodoList([ ...todoList, todoItem ])
+    console.log(todoList)
+  }
+
   return (
     <View style={styles.container}>
       <View>
         <TextInput  
           placeholder="Enter Todo Item"
-            style={ styles.textInput }
+          style={ styles.textInput }
+          // onChangeText={ text => console.log(text) }
+          onChangeText={ text => setTodoItem(text)}
+          value={ todoItem }
           />
         <Button
           title="Add Todo"
-          onPress={() => console.log("Button Clicked")}
+          // onPress={() => console.log("Button Clicked")}
+          onPress={ addTodoList }
         />
       </View>
       <View>
